111cabdc37376f1d2d8a996c347bcd7349492413
Cerus <zocks1703@web.de>
2020-03-21 02:27:22 +0100

    Add username(s) -> uuid(s)

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,5 @@
 class Main {
     static public function main() {
-        trace("Hello, world!");
+
     }
 }
--- src/mojaxe/MojaxeClient.hx
+++ src/mojaxe/MojaxeClient.hx
@@ -1,9 +1,10 @@
 package mojaxe;
 
-import mojaxe.struct.Structs.UsernameTimestamp;
-import mojaxe.struct.Structs.UuidUsername;
 import haxe.Int64;
 import mojaxe.status.ApiStatus;
+import mojaxe.struct.Structs.FullNameResponse;
+import mojaxe.struct.Structs.UsernameTimestamp;
+import mojaxe.struct.Structs.UuidUsername;
 
 interface MojaxeClient {
 
@@ -12,5 +13,9 @@ interface MojaxeClient {
     public function getUuid(username:String, ?at:Int64):Response<UuidUsername>; //Int64.ofInt(Std.int((Sys.time()*1000)));
 
     public function getNameHistory(uuid:UUID):Response<Array<UsernameTimestamp>>;
+
+    public function getUuid(username:String):Response<FullNameResponse>;
+
+    public function getUuids(usernames:Array<String>):Response<Array<FullNameResponse>>;
 
 }
